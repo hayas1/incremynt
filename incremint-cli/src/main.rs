1710499d16438a893590d3d13f7f22ce0a45e877
hayas1 <h4ystack@gmail.com>
2024-12-28 15:10:16 +0000

    default arg

--- incremint-cli/src/main.rs
+++ incremint-cli/src/main.rs
@@ -1,12 +1,23 @@
+use chrono::{Datelike, Local};
 use clap::Parser;
 use incremint::{digit::Digits, increment::Incremint};
 
 #[derive(Parser)]
 pub struct Cli {
     #[arg(short, long)]
+    #[clap(default_value_t = Self::this_year())]
     prev: usize,
     #[arg(short, long)]
+    #[clap(default_value_t = Self::next_year())]
     next: usize,
+}
+impl Cli {
+    pub fn this_year() -> usize {
+        Local::now().year() as usize
+    }
+    pub fn next_year() -> usize {
+        Self::this_year() + 1
+    }
 }
 
 fn main() {
