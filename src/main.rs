eed1e9a249e848e7dda1fabecfcc10d527b37bc8
hayas1 <h4ystack@gmail.com>
2024-12-28 22:54:04 +0900

    2 -> 3

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
-use incremint::show::{Digit, Digits};
+use incremint::show::{Digit, Digits, Incremint};
 
 fn main() {
-    let year = Digits(vec![
+    let prev = Digits(vec![
         Digit(incremint::show::TWO),
         Digit(incremint::show::ZERO),
         Digit(incremint::show::TWO),
         Digit(incremint::show::FIVE),
     ]);
-    println!("{}", year);
+    let next = Digits(vec![
+        Digit(incremint::show::THREE),
+        Digit(incremint::show::ZERO),
+        Digit(incremint::show::TWO),
+        Digit(incremint::show::FIVE),
+    ]);
+    println!("{}", Incremint { prev, next });
 }
--- src/show.rs
+++ src/show.rs
@@ -1,5 +1,17 @@
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default, Hash)]
 pub struct Digit(pub [[char; 4]; 6]);
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default, Hash)]
 pub struct Digits(pub Vec<Digit>);
+
+pub const SPACE: [[char; 4]; 6] = [
+    [' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' '],
+];
 
 pub const ZERO: [[char; 4]; 6] = [
     ['┏', '━', '━', '┓'],
@@ -111,6 +123,81 @@ impl std::fmt::Display for Digits {
             for row in &self.0 {
                 for c in row.0[i] {
                     write!(f, "{}", c)?;
+                }
+            }
+            writeln!(f)?;
+        }
+        Ok(())
+    }
+}
+impl Digits {
+    pub fn padding(&self, p: usize) -> Self {
+        if p > self.0.len() {
+            let mut prefix = Digits(vec![Digit(SPACE); p - self.0.len()]);
+            for d in &self.0 {
+                prefix.0.push(d.clone());
+            }
+            prefix
+        } else {
+            self.clone()
+        }
+    }
+}
+
+pub struct Incremint {
+    pub prev: Digits,
+    pub next: Digits,
+}
+impl std::fmt::Display for Incremint {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for i in 0..8 {
+            if i < 1 {
+                for (rp, rn) in self.prev.0.iter().zip(self.next.0.iter()) {
+                    if rp == rn {
+                        for j in 0..4 {
+                            write!(f, "{}", SPACE[i + 2][j])?;
+                        }
+                    } else {
+                        for c in rn.0[i + 2] {
+                            write!(f, "{}", c)?;
+                        }
+                    }
+                }
+            } else if i < 4 {
+                for (rp, rn) in self.prev.0.iter().zip(self.next.0.iter()) {
+                    if rp == rn {
+                        for j in 0..4 {
+                            write!(f, "{}", rp.0[i - 1][j])?;
+                        }
+                    } else {
+                        for c in rn.0[i + 2] {
+                            write!(f, "{}", c)?;
+                        }
+                    }
+                }
+            } else if i < 7 {
+                for (rp, rn) in self.prev.0.iter().zip(self.next.0.iter()) {
+                    if rp == rn {
+                        for j in 0..4 {
+                            write!(f, "{}", rp.0[i - 1][j])?;
+                        }
+                    } else {
+                        for c in rp.0[i - 4] {
+                            write!(f, "{}", c)?;
+                        }
+                    }
+                }
+            } else if i < 8 {
+                for (rp, rn) in self.prev.0.iter().zip(self.next.0.iter()) {
+                    if rp == rn {
+                        for j in 0..4 {
+                            write!(f, "{}", SPACE[i - 2][j])?;
+                        }
+                    } else {
+                        for c in rp.0[i - 4] {
+                            write!(f, "{}", c)?;
+                        }
+                    }
                 }
             }
             writeln!(f)?;
