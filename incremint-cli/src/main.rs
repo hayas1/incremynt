875c93bb06ce0ada5683f121419aaea335bf642b
hayas1 <h4ystack@gmail.com>
2024-12-28 15:01:23 +0000

    clap

--- incremint-cli/src/main.rs
+++ incremint-cli/src/main.rs
@@ -1,7 +1,16 @@
+use clap::Parser;
 use incremint::{digit::Digits, increment::Incremint};
 
+#[derive(Parser)]
+pub struct Cli {
+    #[arg(short, long)]
+    prev: usize,
+    #[arg(short, long)]
+    next: usize,
+}
+
 fn main() {
-    let prev = Digits::from(2024);
-    let next = Digits::from(3024);
+    let cli = Cli::parse();
+    let (prev, next) = (Digits::from(cli.prev), Digits::from(cli.next));
     println!("{}", Incremint { prev, next });
 }
