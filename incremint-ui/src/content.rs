da8c99af1f0ef3a9fb6a096e0d26542767a91ba4
hayas1 <h4ystack@gmail.com>
2024-12-29 10:31:15 +0000

    usize input

--- incremint-ui/src/content.rs
+++ incremint-ui/src/content.rs
@@ -1,9 +1,18 @@
+use incremint::{increment::Incremint, interface::Application, space::Width};
+use web_sys::HtmlInputElement;
 use yew::prelude::*;
 use yew_autoprops::autoprops;
 
 #[autoprops]
 #[function_component(Content)]
 pub fn content() -> HtmlResult {
+    let program = Application::<Incremint> {
+        d: (0, 0).into(),
+        space: Width::Full,
+        scale: 1,
+    };
+    let prev = use_state(|| 2024);
+    let next = use_state(|| 3024);
     Ok(html! {
         <div class="container w-full h-full mx-auto">
             <div class="h-[50vh] flex justify-center items-center">
@@ -18,9 +27,43 @@ pub fn content() -> HtmlResult {
                     {"┃┏━┛            "} <br />
                 </div>
             </div>
-            <div class="">
-                { for (0..20).map(|_| html! { <p>{ "Incremental number generator" }</p> }) }
+            <div class="flex justify-center pt-4">
+                <div class="flex-initial mx-2"> <UsizeInput label="prev" value_handler={prev.clone()} /> </div>
+                <div class="flex-initial mx-2"> <UsizeInput label="next" value_handler={next.clone()} /> </div>
             </div>
+            <p>{ *prev }</p>
+            <p>{ " → " }</p>
+            <p>{ *next }</p>
+        </div>
+    })
+}
+
+#[autoprops]
+#[function_component(UsizeInput)]
+pub fn usize_input(label: &String, value_handler: &UseStateHandle<usize>) -> HtmlResult {
+    let onchange = {
+        let value_handler = value_handler.clone();
+        Callback::from(move |e: Event| {
+            let Some(input): Option<HtmlInputElement> = e.target_dyn_into() else {
+                return gloo_console::error!("target is not HtmlInputElement");
+            };
+            let Ok(value) = input.value().parse() else {
+                return gloo_console::error!("fail to parse value");
+            };
+            value_handler.set(value);
+        })
+    };
+
+    let initial = *value_handler.clone();
+    let input_id = format!("input-int-{}", label);
+
+    Ok(html! {
+        <div class="flex items-center border-b border-slate-500 px-2">
+            <label for={input_id.clone()} class="text-sm text-right text-slate-500 dark:text-slate-50">{ label }</label>
+            <input type="number" value={initial.to_string()} min="0" id={input_id.clone()} onchange={onchange}
+                class="border-none rounded-sm bg-transparent text-center text-slate-900 dark:text-slate-50 leading-tight
+                    focus:outline-none focus:shadow-outline appearance-none"
+            />
         </div>
     })
 }
